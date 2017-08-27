fa642c1cbf0ae232670e6d71565204782deb12a7
Thiago Feijó <thiago.feijo13@gmail.com>
2017-08-27 03:44:20 -0300

    add index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const Model = require('../../lib/facade');
+var gcm = require('node-gcm');
+var tokensAndroid = [];
+var senderID = 'your_sender_ID';
+
+class pushoModel extends Model {
+  
+  addToken(token) {
+    tokensAndroid.push(token);
+  }
+  
+  send(messagep){
+    var msgAndroid = {'title': messagep.titulo, 'body': messagep.msg, 'icon':'ic_launcher'};
+
+    var message = new gcm.Message({
+      data: {
+        key1: 'msg'
+      },
+      notification: msgAndroid
+    });
+
+    var sender = new gcm.Sender(senderID);
+    tokensAndroid = Array.from(new Set(tokensAndroid));
+
+    sender.sendNoRetry(message, { registrationTokens: tokensAndroid }, function(err, response) {
+      if(err) console.error(err);
+      else    console.log(response);
+    });
+  }
+  
+}
+
+module.exports = new pushoModel();
